60db0de848873d2cf285f78e18e94c4661684792
Ruilin Huang <cxghrl@gmail.com>
2016-06-06 07:02:12 +0800

    fix: SICP solution 3.51

--- ch3/3.51.scm
+++ ch3/3.51.scm
@@ -1,14 +1,3 @@
-(define (stream-ref s n)
-  (if (= n 0)
-      (stream-car s)
-      (stream-ref (stream-cdr s) (- n 1))))
-
-(define (stream-for-each proc s)
-  (if (stream-null? s)
-      'done
-      (begin (proc (stream-car s))
-             (stream-for-each proc (stream-cdr s)))))
-
 (define (display-line x)
   (newline)
   (display x))
