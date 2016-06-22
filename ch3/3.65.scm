9c9f32085d43314e760a657d071a5885508cefa6
Ruilin Huang <cxghrl@gmail.com>
2016-06-22 08:28:16 +0800

    add: SICP solution 3.65

---
+++ ch3/3.65.scm
@@ -0,0 +1,29 @@
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s
+               (make-tableau transform
+                             (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+              (make-tableau transform s)))
+
+(define (ln2-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (ln2-summands (+ n 1)))))
+
+(define ln2-stream
+  (partial-sums (ln2-summands 1)))
+
+(define ln2-euler-stream
+  (euler-transform ln2-stream))
+
+(define ln2-accelerated-euler-stream
+  (accelerated-sequence euler-transform ln2-stream))
